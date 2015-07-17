71359703362938a17830fafc07e1d0c67ad0d43b
John Mitchell <jlmitch5@ncsu.edu>
2015-07-17 16:41:30 -0400

    add drawer sandbox

---
+++ _sandboxes/drawer/drawer.js
@@ -0,0 +1,37 @@
+// calculate the percentage of drawer to show
+// based on the display of the nav
+$.fn.setDrawerHeight = function(navClass, seed) {
+    seed = seed || 0;
+    
+    function newDrawerHeight(nav, drawer, top) {
+        var show = nav - top,
+            hidePercentage;
+
+        hidePercentage = (show > 0) ? show/nav : (top >= 0) ? 0 : 1;
+        return drawer - drawer * hidePercentage;
+    }
+
+    var nav = $(navClass).height(),
+        drawerElem = this,
+        drawer = drawerElem.height(),
+        offset = $(navClass).offset().top;
+
+    var top = $(window).scrollTop() + seed - offset;
+    drawerElem.height(newDrawerHeight(nav, drawer, top));
+    $(window).scroll(function() {
+        var top = $(window).scrollTop() + seed - offset;
+        drawerElem.height(newDrawerHeight(nav, drawer, top));
+    });
+};
+
+$(document).ready(function() {
+    $(".ScrollShow-rightDrawer3").setDrawerHeight(".ScrollShow-secondDrawer");
+    $(".ScrollShow-rightDrawer2").setDrawerHeight(".ScrollShow-drawer");
+    $(".ScrollShow-secondDrawer3").setDrawerHeight(".ScrollShow-notch");
+    $(".ScrollShow-secondDrawer2").setDrawerHeight(".ScrollShow-cover");
+    $(".ScrollShow-secondDrawer").setDrawerHeight(".ScrollShow-static");
+    $(".ScrollShow-navColor").setDrawerHeight(".ScrollShow-nav", 50);
+    $(".ScrollShow-notch").setDrawerHeight(".ScrollShow-nav");
+    $(".ScrollShow-cover").setDrawerHeight(".ScrollShow-nav");
+    $(".ScrollShow-drawer").setDrawerHeight(".ScrollShow-nav");
+});
